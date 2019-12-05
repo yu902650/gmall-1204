a8121eea721452827f2a543010a214f2c4681d61
BoYu <974619916@qq.com>
2019-12-05 14:24:11 +0800

    log

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -6,6 +6,12 @@ import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * dubbo原理：
+ *  1）rpc 两个不同的服务（进程，机器）。建立连接，传输数据
+ *  2）dubbo的那张流程图，服务器提供，消费，注册中心，监控中心
+ *
+ */
 @SpringBootApplication
 @EnableDubbo
 @MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
